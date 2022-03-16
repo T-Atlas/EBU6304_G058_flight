688fd0923700477e33257e665216a78faf28f95b
MASON <1282676427@qq.com>
2022-03-17 01:44:16 +0800

    关于乘客的相关实体类 版本：0.1

---
+++ src/main/java/entity/BoardingPass.java
@@ -0,0 +1,84 @@
+package entity;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+/**
+ * @author SongBo
+ */
+public class BoardingPass {
+    private Passenger passenger;
+    private Flight flight;
+    private String seatNumber;
+    private String foodType;
+    private int boardingGate;
+    private int boardingNo;
+
+    public enum typeOfMeal {
+        //Three types of meal
+        STANDARD, VEGETARIAN, HALAL
+    }
+
+    public BoardingPass() {
+    }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(Passenger passenger) {
+        this.passenger = passenger;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public String getFoodType() {
+        return foodType;
+    }
+
+    public void setFoodType(String foodType) {
+        this.foodType = foodType;
+    }
+
+    public int getBoardingGate() {
+        return boardingGate;
+    }
+
+    public void setBoardingGate(int boardingGate) {
+        this.boardingGate = boardingGate;
+    }
+
+    public int getBoardingNo() {
+        return boardingNo;
+    }
+
+    public void setBoardingNo(int boardingNo) {
+        this.boardingNo = boardingNo;
+    }
+
+    @Override
+    public String toString() {
+        return "BoardingPass{" +
+                "passenger=" + passenger +
+                ", flight=" + flight +
+                ", seatNumber='" + seatNumber + '\'' +
+                ", foodType='" + foodType + '\'' +
+                ", boardingGate=" + boardingGate +
+                ", boardingNo=" + boardingNo +
+                '}';
+    }
+}
---
+++ src/main/java/entity/Tag.java
@@ -0,0 +1,76 @@
+package entity;
+
+/**
+ * @author SongBo
+ */
+public class Tag {
+    private Passenger passenger;
+    private Flight flight;
+    private int tagNo;
+    private int baggageNum;
+    private int baggageId;
+    private double weight;
+
+    public Tag() {
+    }
+
+    public Passenger getPassenger() {
+        return passenger;
+    }
+
+    public void setPassenger(Passenger passenger) {
+        this.passenger = passenger;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+
+    public int getTagNo() {
+        return tagNo;
+    }
+
+    public void setTagNo(int tagNo) {
+        this.tagNo = tagNo;
+    }
+
+    public int getBaggageNum() {
+        return baggageNum;
+    }
+
+    public void setBaggageNum(int baggageNum) {
+        this.baggageNum = baggageNum;
+    }
+
+    public int getBaggageId() {
+        return baggageId;
+    }
+
+    public void setBaggageId(int baggageId) {
+        this.baggageId = baggageId;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Tag{" +
+                "passenger=" + passenger +
+                ", flight=" + flight +
+                ", tagNo=" + tagNo +
+                ", baggageNum=" + baggageNum +
+                ", baggageId=" + baggageId +
+                ", weight=" + weight +
+                '}';
+    }
+}
