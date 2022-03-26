6b57776919fe2c03b6eed8bd8c8cdde16e012c2e
Lian Junhong <194668285@qq.com>
2022-03-26 22:41:12 +0800

    Build structures of project files

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,7 +1,9 @@
 import org.junit.jupiter.api.Test;
+import com.app.fight.entity.Passenger;
 
 public class HelloTest {
 
+    Passenger passenger;
     @Test
     public void HelloTestMain() {
         System.out.println("hello");
