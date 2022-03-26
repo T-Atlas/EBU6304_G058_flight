d8ca28aca36f88023b62cb06fca9858990435970
BoranJia <3580046015@qq.com>
2022-03-27 00:35:26 +0800

    Admin

---
+++ src/main/java/com/app/flight/entity/Admin.java
@@ -0,0 +1,37 @@
+package com.app.flight.entity;
+
+/**
+ * @author JiaBoran
+ */
+public class Admin {
+    private int adminId;
+    private String password;
+
+    public Admin(){
+    }
+
+    public int getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Admin{" +
+                "adminId=" + adminId +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
+
