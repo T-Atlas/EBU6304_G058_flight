294c314229d572943ba3f0f501be3c83c7445af7
Lian Junhong <194668285@qq.com>
2022-03-27 15:30:28 +0800

    Updated the first interface on language selection in line with the prototype design.

--- src/main/java/com/app/flight/entity/Company.java
+++ src/main/java/com/app/flight/entity/Company.java
@@ -2,15 +2,24 @@ package com.app.flight.entity;
 
 /**
  * @author Huanghong
+ * @author LianJunhong
+ * @version 0.1
+ * @date 2022.3.27
  */
 public class Company {
-    //the order number of company
+    /**
+     * the order number of company
+     */
     private String companyId;
 
-    //the name of the airline company
+    /**
+     * the name of the airline company
+     */
     private String name;
 
-    //the introduction of the airline company
+    /**
+     * the introduction of the airline company
+     */
     private String introduction;
 
     public String getCompanyId() {
--- src/main/java/com/app/flight/entity/Staff.java
+++ src/main/java/com/app/flight/entity/Staff.java
@@ -3,6 +3,9 @@ package com.app.flight.entity;
 
 /**
  * @author SongBo
+ * @author LianJunhong
+ * @version 0.1
+ * @date 2022.3.27
  */
 public class Staff {
 }
