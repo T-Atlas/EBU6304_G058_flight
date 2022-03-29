ee61ece43c4d6e9f89cbeea2254c4f1bba1b8eb6
Lian Junhong <194668285@qq.com>
2022-03-29 22:35:31 +0800

    update module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,10 @@
 module com.app.fight {
     requires javafx.controls;
     requires javafx.fxml;
+    requires com.fasterxml.jackson.annotation;
+    requires com.fasterxml.jackson.core;
+    requires com.fasterxml.jackson.databind;
+    requires com.fasterxml.jackson.datatype.jsr310;
 
 
     opens com.app.flight to javafx.fxml;
