c6fe18b8f96c7865e1ea65e669d1396111fe3362
HH <693906439@qq.com>
2022-04-01 22:26:17 +0800

    resource

--- src/main/java/com/app/flight/Main.java
+++ src/main/java/com/app/flight/Main.java
@@ -16,7 +16,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/selectLanguage.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/printTags.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Welcome!");
         stage.setScene(scene);
---
+++ src/main/java/com/app/flight/controller/FoodTypeController.java
@@ -0,0 +1,41 @@
+package com.app.flight.controller;
+
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+/**
+ * This code is used to select the food type.
+ * @author HuangHong
+ * @version 0.1
+ * @date 2022.3.31
+ */
+public class FoodTypeController {
+
+    @FXML
+    private Button next;
+
+    /**
+     * The code for button "next" in ” SelectFoodType.fxml“
+     * When click the button, change to “Check-In.fxml”
+     */
+    public void nextClick(ActionEvent actionEvent) {
+        Platform.runLater(() -> {
+            try {
+                new FinishController().start(new Stage());//需要修改成页面展示的controller
+                ((Stage) (next.getScene().getWindow())).close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public void isSelected(){
+        
+    }
+
+}
