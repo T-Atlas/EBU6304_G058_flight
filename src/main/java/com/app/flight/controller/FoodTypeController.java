f8f1a0b1b6cc24b912193532dc78aaddc16cd512
HH <693906439@qq.com>
2022-04-03 22:02:58 +0800

    4.3

--- src/main/java/com/app/flight/controller/FoodTypeController.java
+++ src/main/java/com/app/flight/controller/FoodTypeController.java
@@ -1,9 +1,13 @@
 package com.app.flight.controller;
 
+import com.app.flight.Main;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,15 +22,56 @@ public class FoodTypeController {
 
     @FXML
     private Button next;
+    @FXML
+    private RadioButton r1;
+    @FXML
+    private RadioButton r2;
+    @FXML
+    private RadioButton r3;
+
+    private String type = null;
+
+
+    /**
+     * To judge which food type is the passenger selected
+     * @return
+     */
+    public String foodType(){
+
+        r1.setUserData("Standard");
+        r2.setUserData("Vegetarian");
+        r3.setUserData("Halal");
+
+        if(r1.isSelected()){
+            r2.setSelected(false);
+            r3.setSelected(false);
+            type = r1.getUserData().toString();
+        }if(r2.isSelected()){
+            r1.setSelected(false);
+            r3.setSelected(false);
+            type = r2.getUserData().toString();
+        }if(r3.isSelected()){
+            r1.setSelected(false);
+            r2.setSelected(false);
+            type = r3.getUserData().toString();
+        }
+        return type;
+
+    }
 
     /**
      * The code for button "next" in ” SelectFoodType.fxml“
-     * When click the button, change to “Check-In.fxml”
+     * When click the button, change to “CheckInResult.fxml”
      */
     public void nextClick(ActionEvent actionEvent) {
+
+        //invoke foodType() method to get the user's choice of food type
+        type = foodType();
+        System.out.println(type);
+
         Platform.runLater(() -> {
             try {
-                new FinishController().start(new Stage());//需要修改成页面展示的controller
+                new PrintTagsController().start(new Stage());//需要修改成页面展示的controller
                 ((Stage) (next.getScene().getWindow())).close();
             } catch (IOException e) {
                 e.printStackTrace();
@@ -34,8 +79,16 @@ public class FoodTypeController {
         });
     }
 
-    public void isSelected(){
-        
+    /**
+     * The code for other pages to open SelectFoodType.fxml
+     */
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("fxml/SelectFoodType.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
+        stage.setTitle("Smart flight check-in kiosk");
+        stage.setScene(scene);
+        stage.show();
     }
 
 }
