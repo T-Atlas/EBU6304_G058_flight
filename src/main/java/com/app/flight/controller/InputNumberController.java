64ffb9c806c35e0378db9cac0690fa18554dc3f3
Lian Junhong <194668285@qq.com>
2022-04-04 17:55:50 +0800

    relocated file

--- src/main/java/com/app/flight/controller/InputNumberController.java
+++ src/main/java/com/app/flight/controller/InputNumberController.java
@@ -1,7 +1,20 @@
 package com.app.flight.controller;
 
+import com.app.flight.entity.Passenger;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 /**
  * @author LianJunhong
  */
 public class InputNumberController {
+    @FXML
+    public TextField id;
+    getFoodtype getFoodtype1= new getfoodimpl();
+
+    public void submit(ActionEvent actionEvent) {
+        Passenger p = getFoodtype1.lookupPassenger(id.getText());
+        System.out.println(p);
+    }
 }
---
+++ src/main/java/com/app/flight/controller/getFoodtype.java
@@ -0,0 +1,7 @@
+package com.app.flight.controller;
+
+import com.app.flight.entity.Passenger;
+
+public interface getFoodtype {
+    public Passenger lookupPassenger(String ID);
+}
