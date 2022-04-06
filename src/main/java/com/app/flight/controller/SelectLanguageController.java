4158e0ea20a89d71b60d8d48bb7c54b2b78082c6
Lian Junhong <194668285@qq.com>
2022-04-06 22:39:33 +0800

    4.6 add page

--- src/main/java/com/app/flight/controller/SelectLanguageController.java
+++ src/main/java/com/app/flight/controller/SelectLanguageController.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
