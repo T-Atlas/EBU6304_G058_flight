6a4b8d700cf40b77f46964f2e27bab79688299c3
MASON <1282676427@qq.com>
2022-04-07 20:22:56 +0800

    update service: add GetReservation

---
+++ src/main/java/com/app/flight/service/impl/GetReservationImpl.java
@@ -0,0 +1,13 @@
+package com.app.flight.service.impl;
+
+import com.app.flight.entity.Reservation;
+import com.app.flight.service.GetReservation;
+
+import java.util.ArrayList;
+
+public class GetReservationImpl implements GetReservation {
+    @Override
+    public ArrayList<Reservation> lookupReservations(String id) {
+        return null;
+    }
+}
