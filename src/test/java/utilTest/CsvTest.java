235d70dba4436e85ed79e9cf2ab9c43698562935
MASON <1282676427@qq.com>
2022-05-01 20:06:33 +0800

    5.1 update fastjson snapshot

--- src/test/java/utilTest/CsvTest.java
+++ src/test/java/utilTest/CsvTest.java
@@ -20,12 +20,12 @@ public class CsvTest {
     public void csvTest() {
         passenger = new Passenger();
         passenger.setPassengerId("220802200005217777");
-        passenger.setFirstName("Test");
+        passenger.setFirstName("NonStringTest");
         passenger.setLastName("Jun");
         passenger.setTelephone("13104368848");
         passenger.setAge(22);
         String filePath1 = "src/main/resources/com/app/flight/data/csv/Passenger.csv";
-        Csv.updateCsv(passenger, filePath1);
+        //Csv.addCsv(passenger, filePath1, true);
 
         flight = new Flight();
         flight.setFlightId("MH1234");
@@ -37,7 +37,7 @@ public class CsvTest {
         flight.setArrivalTime(LocalDateTime.of(2022, 5, 25, 14, 55));
 
         String filePath2 = "src/main/resources/com/app/flight/data/csv/Flight.csv";
-        Csv.addCsv(flight, filePath2, true);
+        //Csv.addCsv(flight, filePath2, true);
 
         Snowflake snowflake = IdUtil.getSnowflake(1, 1);
         String id = snowflake.nextIdStr();
@@ -59,19 +59,19 @@ public class CsvTest {
     public void foodCsvTest() {
         Food food1 = new Food();
         food1.setFoodName(Food.foodType.STANDARD);
-        food1.setFoodPrice(Double.parseDouble("30"));
+        food1.setFoodPrice(30.0);
 
         Food food2 = new Food();
         food2.setFoodName(Food.foodType.HALAL);
-        food2.setFoodPrice(Double.parseDouble("50"));
+        food2.setFoodPrice(50.0);
 
         Food food3 = new Food();
         food3.setFoodName(Food.foodType.VEGETARIAN);
-        food3.setFoodPrice(Double.parseDouble("20"));
+        food3.setFoodPrice(20.0);
 
         String filePath = "src/main/resources/com/app/flight/data/csv/Food.csv";
-        Csv.updateCsv(food1, filePath);
-        Csv.updateCsv(food2, filePath);
-        Csv.updateCsv(food3, filePath);
+        Csv.addCsv(food1, filePath, false);
+        Csv.addCsv(food2, filePath, false);
+        Csv.addCsv(food3, filePath, false);
     }
 }
