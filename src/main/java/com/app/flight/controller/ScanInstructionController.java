f682c714d77c7a1c8d640880c3b2a93295a32ae3
Lian Junhong <194668285@qq.com>
2022-05-02 19:35:40 +0800

    for zhenghan

--- src/main/java/com/app/flight/controller/ScanInstructionController.java
+++ src/main/java/com/app/flight/controller/ScanInstructionController.java
@@ -1,4 +1,11 @@
 package com.app.flight.controller;
 
+/**
+ * @author LianJunhong
+ */
 public class ScanInstructionController {
+    //TODO: 狗郑瀚，这里需要在用户点击Scan那个按钮后，调用一个界面展示扫描只是视频，视频在video目录下。然后用户可以选择跳过或者在视频播放完毕以后再次播放。
+    //TODO：然后进入下一个控制类，模拟一个进度条，然后调用Scanner方法模拟读取用户扫描数据。最后回到InfoConfirmController类。
+
+
 }
