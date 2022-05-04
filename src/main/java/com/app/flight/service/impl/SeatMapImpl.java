d462b07edd9d50edb874d0255435d06f38d84940
Lian Junhong <194668285@qq.com>
2022-05-04 23:01:58 +0800

    Refactor SeatMapImpl with GetSeatMapImpl and SetSeatMapImpl

--- src/main/java/com/app/flight/service/impl/SeatMapImpl.java
+++ src/main/java/com/app/flight/service/impl/SeatMapImpl.java
@@ -1,24 +1,30 @@
 package com.app.flight.service.impl;
 
+import com.app.flight.service.GetSeatMap;
 import com.app.flight.service.SetSeatMap;
+import com.app.flight.util.Csv;
 import com.app.flight.util.Json;
 import com.app.flight.util.Seat;
 
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * @author SongBo
- * @version 1.0
+ * @author SongBo
+ * @author LianJunhong
+ * @version 1.1
+ * @date 2022.4.21
  * @date 2022.4.22
  */
-public class SetSeatMapImpl implements SetSeatMap {
+public class SeatMapImpl implements SetSeatMap, GetSeatMap {
     @Override
     public void updateSeatMap(String flightId, String column, int row) {
-        GetSeatMapImpl getSeatMap = new GetSeatMapImpl();
-        Map<Integer, Map<String, Boolean>> seatMap = getSeatMap.lookupSeatMap(flightId);
+        Map<Integer, Map<String, Boolean>> seatMap = lookupSeatMap(flightId);
         Map<String, Boolean> stringBooleanMap = seatMap.get(row);
         Boolean status = stringBooleanMap.get(column);
         status = !status;
@@ -37,5 +43,22 @@ public class SetSeatMapImpl implements SetSeatMap {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public Map<Integer, Map<String, Boolean>> lookupSeatMap(String flightId) {
+        ArrayList<String[]> seatData = Csv.readCsv(Seat.generateSeatFilePath(flightId));
+        Map<Integer, Map<String, Boolean>> seatMap = new HashMap<>(seatData.size());
+        String[] columnString = new String[]{"A", "B", "C", "D", "E", "F"};
+        int j = 1;
+        for (String[] seatStatus : seatData) {
+            Map<String, Boolean> rowMap = new HashMap<>(6);
+            for (int i = 0; i < seatStatus.length; i++) {
+                rowMap.put(columnString[i], Boolean.valueOf(seatStatus[i]));
+            }
+            seatMap.put(j, rowMap);
+            j++;
+        }
+        return seatMap;
     }
 }
