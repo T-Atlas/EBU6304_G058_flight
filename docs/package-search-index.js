b3d13448c8940ac85c6a237622d58f70b8ddd535
Lian Junhong <194668285@qq.com>
2022-05-24 13:08:58 +0800

    update javadocs add unit test cases and automatic uitest cases

---
+++ docs/package-search-index.js
@@ -0,0 +1,5 @@
+packageSearchIndex = [{"l": "com.app.flight"}, {"l": "com.app.flight.controller"}, {"l": "com.app.flight.entity"}, {"l": "com.app.flight.service"}, {"l": "com.app.flight.service.external"}, {"l": "com.app.flight.service.impl"}, {"l": "com.app.flight.service.temp"}, {"l": "com.app.flight.util"}, {
+    "l": "所有程序包",
+    "u": "allpackages-index.html"
+}];
+updateSearchResults();
